11af65c4aa37052fdfb19a6e2f4814e1c8f54055
tyeon99 <xodus170@naver.com>
2026-07-30 14:54:44 +0900

    소개페이지 분리

---
+++ app/pages/factor-analyst/intro/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="wrap">
+    <CloseHeader />
+    <main id="main" class="main">
+      <IntroContent />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+// style
+import '~/assets/css/factor-analyst/common.css'
+import CloseHeader from '~/components/factor-analyst/header/CloseHeader.vue';
+import IntroContent from '~/components/factor-analyst/intro/IntroContent.vue';
+
+</script>
