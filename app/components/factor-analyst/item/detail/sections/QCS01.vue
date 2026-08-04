8937ad6ede140de7baccd1fee53d54d160334e24
tyeon99 <xodus170@naver.com>
2026-08-04 16:22:34 +0900

    퀄리티 상세보기페이지 수정

--- app/components/factor-analyst/item/detail/sections/QCS01.vue
+++ app/components/factor-analyst/item/detail/sections/QCS01.vue
@@ -25,6 +25,40 @@
         </div>
       </div>
     </div>
+
+    <div class="list-group">
+      <div 
+        v-for="(item, idx) in itemFScore" 
+        :key="idx" 
+        class="list"
+      >
+        <div class="gray-box">
+          <p>{{ item.boxTxt }}</p>
+
+          <div class="range-wrap">
+            <div class="range">
+              <div class="bar">
+                <span class="avg"></span>
+              </div>
+              <div class="score" :style="{ width: `${item.score}%` }">
+                <img
+                  class="item-icon"
+                  width="18"
+                  src="~/assets/img/factor-analyst/main/item-circle.png"
+                  alt="종목 아이콘"
+                />
+              </div>
+            </div>
+            <div class="txt">
+              <span>0</span>
+              <span>전종목 평균</span>
+              <span>12000</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -51,6 +85,33 @@ const fScoreList = [
     question: '작년보다 빚(부채비율)이 줄었나?',
     isPass: false,
     description: '네, 2025년 부채비율은 <strong class="up">+25.21%</strong>이고, 2026년 부채비율은 <strong class="up">+12.12%</strong> 이예요.   '
+  },
+  {
+    question: '바로 갚을 수 있는 돈(유동비율)이 늘었나?',
+    isPass: true,
+    description: '네, 2025년 유동비율은 <strong class="up">+25.21%</strong>이고, 2026년 유동비율은 <strong class="up">+32.12%</strong> 이예요. '
+  },
+  {
+    question: '주식을 새로 찍어내서 가치를 떨어뜨리지 않았나?',
+    isPass: true,
+    description: '네, 신주발행은 없었어요.'
+  },
+  {
+    question: '작년보다 마진(매출총이익률)이 좋아졌나?',
+    isPass: true,
+    description: '네, 2025년 매출총이익률은 <strong class="up">+25.21%</strong>이고, 2026년 매출총이익은 <strong class="up">+32.12%</strong> 이예요.'
+  },
+  {
+    question: '작년보다 재산(자산회전율)을 굴려서 매출을 더 팍팍 일으켰나?',
+    isPass: true,
+    description: '네, 2025년 자산회전율은 <strong class="up">+25.21%</strong>이고, 2026년 자산회전율은 <strong class="up">+32.12%</strong> 이예요.  '
   }
 ]
+
+const itemFScore = ref([
+  {
+    score: 86.2,
+    boxTxt: 'SK하이닉스의 F스코어의 위치는?'
+  }
+])
 </script>
--- app/components/factor-analyst/item/detail/sections/QCS02.vue
+++ app/components/factor-analyst/item/detail/sections/QCS02.vue
@@ -4,6 +4,26 @@
       <span>돈 버는 효율성은 괜찮을까?</span>
       <h1>GP/A는?</h1>
     </div>
+
+    <div class="list">
+      <div class="gpa-title">
+        <p>SK하이닉스의 GP/A는?</p>
+        <span>86.2</span>
+      </div>
+      <div class="gpa-txt !mb-0">
+        매출 총이익 2500억을 총자산 250억으로 나눈 값이예요.
+      </div>
+    </div>
+
+    <div class="list">
+      <div class="gpa-title">
+        <p>SK하이닉스가 속한 반도체 섹터의  평균 GP/A는?</p>
+        <span>86.2</span>
+      </div>
+      <div class="gpa-txt !mb-0">
+        반도체 섹터의 총 종목수는 32종목이고, 가장 높은 GP/A는 99.2, 가장 낮은 GP/A는 35.2 예요.
+      </div>
+    </div>
     
     <div class="list-group">
       <div 
@@ -11,13 +31,6 @@
         :key="idx" 
         class="list"
       >
-        <div class="gpa-title">
-          <p>{{ item.question }}</p>
-          <span>{{ item.gpaScore }}</span>
-        </div>
-        <div class="gpa-txt">
-          {{ item.description }}
-        </div>
         <div class="gray-box">
           <p>{{ item.boxTxt }}</p>
 
@@ -53,18 +66,12 @@ import '~/assets/css/factor-analyst/common.css'
 
 const gpaList = ref([
   {
-    question: 'SK하이닉스의 GP/A 스코어는?',
-    gpaScore: 86.2,
-    description: '매출 총이익 2500억을 총자산 250억으로 나눈 값이예요.',
     score: 86.2,
-    boxTxt: '전 종목 중 SK하이닉스 GP/A의 위치는?'
+    boxTxt: '섹터에서 SK하이닉스의 백분위는?'
   },
   {
-    question: '반도체 섹터의 평균 스코어는?',
-    gpaScore: 74.5,
-    description: '반도체 섹터의 총 종목수는 32종목이고, 가장 높은 스코어는 99.2, 가장 낮은 스코어는 35.2 예요.',
     score: 74.5,
-    boxTxt: '업종 내 SK하이닉스 GP/A의 위치는?'
+    boxTxt: '전체 시장에서 SK하이닉스의 백분위는?'
   }
 ])
 </script>
