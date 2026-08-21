7ef88cf8739771271c28bb482220a783fc2515b3
tyeon99 <xodus170@naver.com>
2026-08-21 09:26:13 +0900

    QA 90번 수정

--- app/app.vue
+++ app/app.vue
@@ -9,15 +9,15 @@ import { onMounted, onUnmounted, ref } from 'vue'
 // Nuxt 내장 useHead를 확실하게 명시적 임포트
 import { useHead } from '@unhead/vue'
 
-const viewportContent = ref<string>('width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no')
+const viewportContent = ref<string>('width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover')
 
 const handleViewport = (): void => {
   if (typeof window === 'undefined') return
 
   if (window.screen.width < 375) {
-    viewportContent.value = 'width=375, user-scalable=no'
+    viewportContent.value = 'width=375, user-scalable=no, viewport-fit=cover'
   } else {
-    viewportContent.value = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+    viewportContent.value = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover'
   }
 }
 
