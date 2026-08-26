70fd46727e0745396fabb53bed8e498be6ec7f19
tyeon99 <xodus170@naver.com>
2026-08-26 14:23:37 +0900

    슬라이드 탭 이동 수정

--- app/pages/factor-analyst/detail/index.vue
+++ app/pages/factor-analyst/detail/index.vue
@@ -4,7 +4,11 @@
     <DetailTop />
     <DetailTab v-model="currentTab" />
     <main id="main" class="main">
-      <div class="detailTab-content">
+      <div 
+        class="detailTab-content select-none"
+        @touchstart="handleTouchStart"
+        @touchend="handleTouchEnd"
+      >
         <StyleItem v-if="currentTab === 'style-item'" />
         <StyleIntro v-else-if="currentTab === 'style-intro'" />
         <StyleRecord v-else-if="currentTab === 'style-record'" />
@@ -20,11 +24,47 @@ import '~/assets/css/factor-analyst/common.css'
 
 import BackHeader from '~/components/factor-analyst/header/BackHeader.vue'
 import DetailTop from '~/components/factor-analyst/detail/DetailTop.vue'
-import DetailTab from '~/components/factor-analyst/detail/DetailTab.vue';
+import DetailTab from '~/components/factor-analyst/detail/DetailTab.vue'
 import StyleItem from '~/components/factor-analyst/detail/StyleItem.vue'
 import StyleIntro from '~/components/factor-analyst/detail/StyleIntro.vue'
 import StyleRecord from '~/components/factor-analyst/detail/StyleRecord.vue'
 
 const currentTab = ref('style-item')
 
+const tabList = ['style-item', 'style-intro', 'style-record']
+
+const startX = ref(0)
+const endX = ref(0)
+const minSwipeDistance = 50
+
+const handleTouchStart = (e: TouchEvent) => {
+  if (e.touches && e.touches[0]) {
+    startX.value = e.touches[0].clientX
+  }
+}
+
+const handleTouchEnd = (e: TouchEvent) => {
+  if (e.changedTouches && e.changedTouches[0]) {
+    endX.value = e.changedTouches[0].clientX
+    processSwipe()
+  }
+}
+
+const processSwipe = () => {
+  const swipeDistance = startX.value - endX.value
+  const currentIdx = tabList.indexOf(currentTab.value)
+
+  if (currentIdx === -1) return
+
+  // 다음 탭으로
+  if (swipeDistance > minSwipeDistance && currentIdx < tabList.length - 1) {
+    const nextTab = tabList[currentIdx + 1]
+    if (nextTab) currentTab.value = nextTab
+  }
+  // 이전 탭으로
+  else if (swipeDistance < -minSwipeDistance && currentIdx > 0) {
+    const prevTab = tabList[currentIdx - 1]
+    if (prevTab) currentTab.value = prevTab
+  }
+}
 </script>
