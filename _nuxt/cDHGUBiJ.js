a96c4e3c11ba7bc5a84424ad75ec3dda02b577e1
tyeon99 <tyeon99@users.noreply.github.com>
2026-09-09 04:03:45 +0000

    deploy: 8741543b89c67f956583de0902f15094e5877ad4

---
+++ _nuxt/cDHGUBiJ.js
@@ -0,0 +1,1 @@
+/* empty css        */import{C as a}from"./BgCGGh8k.js";import{A as o}from"./sXxa_813.js";import{d as t,B as s,E as e,C as n,A as r}from"./CWfDLN5L.js";const i={id:"wrap"},c={id:"main",class:"main"},u=t({__name:"index",setup(m){return(d,p)=>(r(),s("div",i,[e(a),n("main",c,[e(o,{class:"p-5"})])]))}});export{u as default};
