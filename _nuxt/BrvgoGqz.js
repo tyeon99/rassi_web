c2fe7fbeeb3bc7821fb89674669e41e66a565133
tyeon99 <tyeon99@users.noreply.github.com>
2026-09-15 06:31:00 +0000

    deploy: 08aa76ea395ec5393770d4d640c19a55b8f66407

---
+++ _nuxt/BrvgoGqz.js
@@ -0,0 +1,1 @@
+import{C as a}from"./ZnZcJ9vU.js";import{A as o}from"./C_8FDIeG.js";import{d as s,B as t,E as e,C as n,A as r}from"./Cwdj-Pyt.js";import{_ as c}from"./DlAUqK2U.js";const _={id:"wrap"},d={id:"main",class:"main"},i=s({__name:"index",setup(m){return(p,l)=>(r(),t("div",_,[e(a),n("main",d,[e(o,{class:"p-5"})])]))}}),B=c(i,[["__scopeId","data-v-bc5d8a11"]]);export{B as default};
