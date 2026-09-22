0c3409b65b55622d8ccdba2f80b777a3f12f44f8
tyeon99 <xodus170@naver.com>
2026-09-22 13:53:17 +0900

    팩터 swiper 속도 수정

--- app/components/factor-analyst/main/TodayRecommend.vue
+++ app/components/factor-analyst/main/TodayRecommend.vue
@@ -100,7 +100,7 @@ onMounted(() => {
             type: 'fraction'
           },
           autoplay: {
-            delay: 2500,
+            delay: 2000,
             disableOnInteraction: false,
           },
         })
