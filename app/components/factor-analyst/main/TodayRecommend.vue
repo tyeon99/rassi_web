e39df5479fac1ce29dc74ef41f7685c3a12339de
tyeon99 <xodus170@naver.com>
2026-09-22 13:50:12 +0900

    팩터 메인 수정

--- app/components/factor-analyst/main/TodayRecommend.vue
+++ app/components/factor-analyst/main/TodayRecommend.vue
@@ -10,37 +10,37 @@
       고민없이 Pick! 앞으로 한달 강세를 보일 종목을 추천해드려요.
     </div>
     <div class="mainSection__content">
-      <div class="swiper recommendItemSwiper">
-        <div class="swiper-wrapper">
-          <div 
-            v-for="(item, idx) in items" 
-            :key="idx" 
-            class="swiper-slide"
-          >
-            <button
-              class="itemList box01"
-              :class="{ masking: idx == 1 }"
+      <div class="today-recommend">
+        <div class="recommend-title">Pick 종목 현황</div>
+        <div class="recommend-pagination"></div>
+        <div class="swiper recommendItemSwiper">
+          <div class="swiper-wrapper">
+            <div 
+              v-for="(item, idx) in items" 
+              :key="idx" 
+              class="swiper-slide"
             >
-              <div class="recommend-title">Pick 종목 현황</div>
-              <div class="inner-box">
-                <div class="recommend-style">
-                  {{ item.styleName }}에서 추천되었어요.
-                </div>
-                <div class="recommend-item">
-                  <div class="circle">
-                    <img 
-                      width="30" 
-                      src="~/assets/img/factor-analyst/main/item-circle.png" 
-                      alt="종목로고"
-                    >
+              <button
+                class="itemList !p-0"
+                :class="{ masking: idx == 1 }"
+              >
+                <div class="inner-box">
+                  <div class="recommend-style">
+                    {{ item.styleName }}에서 추천되었어요.
                   </div>
-                  <strong>{{ item.name }}</strong>
+                  <div class="recommend-item">
+                    <div class="circle">
+                      <img 
+                        width="30" 
+                        src="~/assets/img/factor-analyst/main/item-circle.png" 
+                        alt="종목로고"
+                      >
+                    </div>
+                    <strong>{{ item.name }}</strong>
+                  </div>
                 </div>
-              </div>
-              <div class="recommend-pagination">
-                <strong>{{ idx + 1 }}</strong>/<span>{{ items.length }}</span>
-              </div>
-            </button>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -60,7 +60,7 @@ import '~/assets/css/factor-analyst/common.css'
 const items = [
   { name: '삼성전자', code: '005930', score: 95.3, styleName: '황금코어 스타일' },
   { name: 'SK하이닉스', code: '000660', score: 92.1, styleName: '황금코어 스타일' },
-  { name: '현대차현대차현대차현대차현대차', code: '005380', score: 89.5, styleName: '황금코어 스타일' },
+  { name: '현대차현대차현대차현대차현대차현대차', code: '005380', score: 89.5, styleName: '황금코어 스타일' },
   { name: '네이버', code: '035420', score: 88.0, styleName: '황금코어 스타일' },
   { name: '카카오', code: '035720', score: 86.4, styleName: '황금코어 스타일' },
   { name: 'LG에너지솔루션', code: '373220', score: 85.1, styleName: '황금코어 스타일' },
@@ -94,7 +94,15 @@ onMounted(() => {
         new globalWithSwiper.Swiper('.recommendItemSwiper', {
           loop: false,
           slidesPerView: 1,
-          spaceBetween: 16
+          spaceBetween: 16,
+          pagination: {
+            el: '.recommend-pagination',
+            type: 'fraction'
+          },
+          autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
         })
         return 
       }
